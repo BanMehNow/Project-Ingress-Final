da8d6539b1d62f83c80ee4f55f34f3b48c3a7778
Aidan <aidanrobertson04@outlook.com>
2026-06-03 13:30:53 +0100

    Made styling changes for my own satiisfaction, removed boilerplate css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,24 @@
 import { useState } from 'react'
-import './App.css'
+import './index.css'
 
 function App() {
-  const [url, setUrl] = useState(' ')
+  const [url, setUrl] = useState('')
   return (
-    
+
     <div className='App'>
       <header className='app-header'>
         <h1>Project Ingress</h1>
       </header>
-
-      <div className='url-input'>
-        <input
-          type="text"
-          value={url}
-          onChange={(input) => setInputValue(input.target.value)}
-          placeholder='Enter source for ingestion...'
-        />
-      </div>
-
+      <main className="ingestion-box">
+        <div className="url-input">
+          <input
+            type="text" 
+            placeholder="Enter source for ingestion..."
+            value={url}
+            onChange={(event) => setUrl(event.target.value)}
+          />
+        </div>
+      </main>
     </div>
   )
 }
