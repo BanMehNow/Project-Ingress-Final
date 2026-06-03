8543d289c4125c5d34773442655de2aaef242704
Aidan <aidanrobertson04@outlook.com>
2026-06-03 13:05:39 +0100

    Added input box for URL ingestion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
+  const [url, setUrl] = useState(' ')
   return (
+    
     <div className='App'>
       <header className='app-header'>
         <h1>Project Ingress</h1>
       </header>
+
+      <div className='url-input'>
+        <input
+          type="text"
+          value={url}
+          onChange={(input) => setInputValue(input.target.value)}
+          placeholder='Enter source for ingestion...'
+        />
+      </div>
+
     </div>
   )
 }
